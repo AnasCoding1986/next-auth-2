a7cbb1e23b71b488a7af1762d424a331c97867c6
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-06 23:02:41 +0600

    first commit

---
+++ src/app/users/[id]/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const page = ({params}) => {
+
+    
+
+    console.log(params);
+    
+
+    return (
+        <div>
+            <h2>This is params</h2>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,49 @@
+"use client";
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import React from 'react';
+
+const Navbar = () => {
+
+    const pathName = usePathname();
+    const router = useRouter();
+
+    const handler = () => {
+        router.push("/about")
+    }
+
+    const links = [
+        {
+            title: "about",
+            path: "/about"
+        },
+        {
+            title: "service",
+            path: "/service"
+        },
+        {
+            title: "contact",
+            path: "/contact"
+        },
+        {
+            title: "users",
+            path: "/users"
+        },
+    ]
+
+    return (
+        <nav className='flex justify-between items-center p-4'>
+            <h2 className='text-xl font-bold'>Next</h2>
+            <ul className='flex'>
+                {
+                    links.map((link) => <li key={link.path}>
+                        <Link className={`${pathName === link.path && "text-red-700"}`} href={link.path}>{link.title}</Link>
+                    </li>)
+                }
+            </ul>
+            <button onClick={handler} className='bg-orange-300 text-4xl p-4'>Login</button>
+        </nav>
+    );
+};
+
+export default Navbar;
