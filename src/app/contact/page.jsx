5349855fa6fd28d067d8bfcdd1a5057c877eb76c
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-09 21:35:53 +0600

    image completed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export const metadata = {
@@ -9,6 +10,14 @@ const page = () => {
     return (
         <div>
             <h3>This is contact page</h3>
+            {/* <Image alt='cion' width="1000" height={"2000"} src='/images/1.png'></Image> */}
+            <div className='grid grid-cols-4'>
+                {
+                    [1,2,3,4]?.map((img) => (
+                        <Image className='border-2 border-black p-2 rounded-2xl' key={img} alt='cion' width="50" height="50" src={`/images/${img}.png`}></Image>
+                    ))
+                }
+            </div>
         </div>
     );
 };
