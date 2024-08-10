cbff4c3402f26b5f777ebbc39e57bc527ce0f5b9
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-10 23:47:22 +0600

    auth solution done

--- src/app/comments/api/auth/[...nextauth]/route.js
+++ src/app/comments/api/auth/[...nextauth]/route.js
@@ -1,14 +1,27 @@
 import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    session: {
         session: {
             strategy: "jwt"
         },
         providers: [
-            
+            CredentialsProvider({
+                credentials: {
+                    email: { label: "Username", type: "text", required: true, placeholder: "Your Email" },
+                    password: { label: "Password", type: "password", required: true,placeholder: "Your Password" }
+                },
+                async authorize(credentials) {
+
+                    if (!credentials) {
+                        return null;
+                    }
+                    return true;
+                }
+            }),
+
         ]
-    }
+    
 })
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
