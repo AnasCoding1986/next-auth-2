0934e9af73280996de3da1cad1453cf02475284c
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-11 22:42:00 +0600

    really confusing

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -12,40 +12,53 @@ const handler = NextAuth({
                 password: { label: "Password", type: "password", placeholder: "password", required: true }
             },
             async authorize(credentials) {
+
+                // const {email,password} = credentials;
+                // console.log(email,password);
+                
+
                 if (!credentials) {
                     return null;
                 }
+                // if (email) {
+                //     const currentUser = users.find((user) => user.email === email);
+                //     if (currentUser) {
+                //         if (currentUser.password === password) {
+                //             return currentUser
+                //         }
+                //     }
+                // }
                 return true;
             }
         })
     ]
-})
+});
 
-const users =[
-    {
-        id: 1,
-        name: "anas",
-        email: "a@a.com",
-        password: "pass"
-    },
-    {
-        id: 2,
-        name: "anas2",
-        email: "b@b.com",
-        password: "pass"
-    },
-    {
-        id: 3,
-        name: "anas3",
-        email: "c@c.com",
-        password: "pass"
-    },
-    {
-        id: 4,
-        name: "anas4",
-        email: "d@d.com",
-        password: "pass"
-    },
-]
+// const users =[
+//     {
+//         id: 1,
+//         name: "anas",
+//         email: "a@a.com",
+//         password: "password"
+//     },
+//     {
+//         id: 2,
+//         name: "anas2",
+//         email: "b@b.com",
+//         password: "password"
+//     },
+//     {
+//         id: 3,
+//         name: "anas3",
+//         email: "c@c.com",
+//         password: "password"
+//     },
+//     {
+//         id: 4,
+//         name: "anas4",
+//         email: "d@d.com",
+//         password: "password"
+//     },
+// ];
 
 export { handler as GET, handler as POST }
