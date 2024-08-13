151373d3427fca0a9163fe0739ce75d2daadf4fd
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-13 14:46:35 +0600

    sign up completed

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,21 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server'
 
 const user = true;
  
 export function middleware(request) {
 
-  const cookie = request.cookies.get('nextjs')
+  // const cookie = request.cookies.get('nextjs');
 
-  if (!cookie) {
-    return NextResponse.rewrite(new URL('/', request.url))
+  const token = cookies(request).get("next-auth.session-token");
+
+  if (!token) {
+    return NextResponse.rewrite(new URL('/api/auth/signin', request.url))
   }
  
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/about', '/dash'],
+  matcher: ['/service', '/dash'],
 }
